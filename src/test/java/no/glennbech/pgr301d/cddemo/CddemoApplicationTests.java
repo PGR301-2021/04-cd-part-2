cd1a8ff29e0aaf826e4eb9f205a7fb1ed5067c20
glennbech <glenn.bech@gmail.com>
2021-09-19 08:33:32 +0200

    initial commit

---
+++ src/test/java/no/glennbech/pgr301d/cddemo/CddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.glennbech.pgr301d.cddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
